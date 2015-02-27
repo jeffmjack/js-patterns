5f51e6f3b408eb02f9f8e4aa61417e11d934014f
Jeff Jackson <jeffmjack@gmail.com>
2015-02-27 00:15:06 -0600

    finish MVO implementation of cat clicker... cool

--- scripts/app.js
+++ scripts/app.js
@@ -1,95 +1,101 @@
-/*
-// clear the screen for testing
-document.body.innerHTML = '';
-document.body.style.background="white";
-*/
+$(function() {
 
-var names = ['jimmy', 'alfonso', 'bart', 'cantrell', 'luis'];
-var pics = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
-var cats = [];
+  var model = {
 
-$(document).ready(function () {
-// loop over the names in our array
-  names.forEach(function (item, index, array) {
-    //fill out the cats array
-    var cat = {
-      name: item,
-      pic:  pics[index],
-      clicks: 0
-    };
-    cats.push(cat);
-});
+    // inital cats 
+    names: ['jimmy', 'alfonso', 'bart', 'cantrell', 'luis'],
+    pics:  ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],
+    cats: [],
 
-  cats.forEach(function (cat, index, array) {
-    // build the li in the aside
+    init: function (names) {
+        model.names.forEach(function (item, index, array) {
+          var cat = {
+            name: item,
+            pic:  model.pics[index],
+            clicks: 0
+          };
+          model.cats.push(cat);
+        });
+    },
+  };
 
-    var $li = $("<li>");
+  var view = {
+    init: function (){
+      view.list.init();
+      view.catPane.init();
+    },
+    list: {
+      init: function  (){
+        view.list.render();
+        $('li').click(function (e){
+          var catID = $(this).attr('id');
+          var clickedCat = octopus.findCatByID(catID);
+          view.catPane.render(clickedCat);
+        });
+      },
 
-    $li.html(cat.name);
-    $li.name = cat.name;
-    $('aside ul').append($li);
-
-    // make clicking a <li> show the article with the li's name in the main
-    $li.click(function(){ 
-      var articleSelector = "article[name=" + $li.name +"]";
-      $('article').hide();
-      $(articleSelector).show();
+      render: function (){
+        var htmlStr = '';
+        octopus.getCats().forEach(function (e, ind, arr){
+          htmlStr = '<li id=\'' +
+            e.name +
+            '\'>' +
+            e.name +
+            '</li>';
+          $('ul').append(htmlStr);
       });
-  });  
+    },
 
-  cats.forEach(function (cat, index, array) {
-    var myArticle = articleMaker(cat); 
-    nameSectionMaker(cat, myArticle);
-    picSectionMaker(cat, myArticle);
-    clicksSectionMaker(cat, myArticle);
+    },
+    catPane: {
+      init: function () {
+        view.catPane.render(octopus.getDefaultCat());
 
-    // hide the cat's profile
- });
+      },
+      render: function (cat) {
+        var myBlock = '';
+        myBlock += '<p> This is ' +
+          cat.name +
+          '!! ' +
+          '<div class=\'picClass\' id= \'' +
+          cat.name +
+          '\'> </div>' + 
+          'total clicks: ' +
+          cat.clicks +
+          '</p>';
+        $('article').html('');
+        $('article').append(myBlock);
+        $('.picClass').css('background-image', 'url(images/'+cat.pic+')');
+        $('.picClass').click(function () {
+          var catID= $(this).attr('id');
+          var cat = octopus.findCatByID(catID);
+          cat.clicks += 1;
+          view.catPane.render(cat);
+        });
 
-    // make clicking on the picSection increment cat.clicks
-    $('.picClass').click(function () {
-      var $myArticle = $(this).closest('article');
-      var name = $myArticle.attr('name');
-      $.grep(cats, function (e) {
-        if (e.name == name) {
-          e.clicks = e.clicks + 1;
-          $myArticle.find('.clicksClass').remove();
-          clicksSectionMaker(e, $myArticle);
-        }
+
+      }
+    },
+  };
+
+  var octopus = {
+    init: function() {
+      model.init();
+      view.init();
+    },
+    getCats: function () {
+      return model.cats;
+    },
+    getDefaultCat: function () {
+      return model.cats[0];
+    },
+    findCatByID: function (catID){
+      var winner = $.grep(model.cats, function (cat){
+        return cat.name === catID; 
       });
-    });
-
-      //build the article in the main 
-    function articleMaker(cat) {
-      var $article = $("<article>");
-      $article.attr('name', cat.name); 
-      $('body').append($article);
-      $article.hide();
-      return $article;
-    }
-
-      // build the cat's name section
-    function nameSectionMaker (cat, $article) {
-      var $nameSection = $('<section>');
-      $nameSection.addClass = 'nameClass';
-      $nameSection.html(cat.name);
-      $article.append($nameSection);
-    }
-
-      // build the cat's pic section
-    function picSectionMaker (cat, $article) {
-      var $picSection = $('<section>');
-      $picSection.addClass('picClass');
-      $picSection.css('background-image', 'url('+cat.pic+')');
-      $article.append($picSection);
-    }
-
-      // build the cat's Clicks section
-    function clicksSectionMaker (cat, $article) {
-      var $clicksSection = $('<section>');
-      $clicksSection.addClass('clicksClass');
-      $clicksSection.html('total clicks: ' + cat.clicks);
-      $article.append($clicksSection);
-    }
+      return winner[0];
+    },
+  };
+octopus.init();
 
 });
